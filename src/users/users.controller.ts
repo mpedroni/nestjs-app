b29a313a5d5db29d1846777d1b80eae4b95d650f
Matheus Pedroni <mpedroni77@gmail.com>
2021-05-11 17:03:28 -0300

    aula 13

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   OnModuleInit,
   Param,
   Post,
+  Put,
 } from '@nestjs/common';
 import { ApiBody } from '@nestjs/swagger';
 import { Client, ClientKafka, Transport } from '@nestjs/microservices';
@@ -31,7 +33,7 @@ export class UsersController implements OnModuleInit {
   private client: ClientKafka;
 
   async onModuleInit() {
-    const requestPatterns = ['find-all-user', 'find-user'];
+    const requestPatterns = ['find-all-user', 'find-user', 'create-user'];
 
     requestPatterns.forEach(async (pattern) => {
       this.client.subscribeToResponseOf(pattern);
@@ -53,5 +55,27 @@ export class UsersController implements OnModuleInit {
   @ApiBody({ type: UserDto })
   create(@Body() user: UserDto) {
     return this.client.emit('create-user', user);
+  }
+
+  @Put(':id')
+  @ApiBody({ type: UserDto })
+  update(
+    @Param() id: number,
+    @Body() { name, email, phone, password }: UserDto,
+  ) {
+    const payload = {
+      id,
+      name,
+      email,
+      phone,
+      password,
+    };
+
+    return this.client.emit('update-user', payload);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.client.emit('delete-user', { id });
   }
 }
